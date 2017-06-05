2f532aaf0c34625234afe469e8244cb77aa7539d
Sebastian Castillo-Hair <castillohair@gmail.com>
2017-06-05 11:18:33 -0500

    Added dependencies to setup.py.

--- setup.py
+++ setup.py
@@ -85,7 +85,9 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
+    install_requires=['numpy>=1.11.3',
+                      'pandas>=0.19.2',
+                      'matplotlib>=2.0.0'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
